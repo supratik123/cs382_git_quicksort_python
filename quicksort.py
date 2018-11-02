93a87787ada5157caeaa6018409fc98b30a3fb80
sacusa <sudhanshugupta96@gmail.com>
2018-11-02 14:45:14 +0530

    Implement quickSort

--- quicksort.py
+++ quicksort.py
@@ -6,4 +6,13 @@ from partition import *
 # low  --> Starting index,
 # high  --> Ending index
 def quickSort(arr, low, high):
-    pass
+    if low < high:
+ 
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr, low, high)
+ 
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
