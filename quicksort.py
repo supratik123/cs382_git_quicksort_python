26e3fbf84c124f330350c440f1015031fe297a30
sacusa <sudhanshugupta96@gmail.com>
2018-11-02 14:44:31 +0530

    Add quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from partition import *
+ 
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low  --> Starting index,
+# high  --> Ending index
+def quickSort(arr, low, high):
+    pass
